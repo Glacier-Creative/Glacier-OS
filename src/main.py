163d92b2c4b980fca164c76c9b6e39f22eef44ae
Johnny Stene <johnny@stene.xyz>
2025-01-19 22:42:59 -0700

    start font rendering

--- src/main.py
+++ src/main.py
@@ -12,6 +12,9 @@ for i in range(0, 64):
     graphics.set_pixel(i, 64, 0x00)
 
 graphics.draw_image(0, 0, "img/amogus.xbm")
+graphics.draw_image(21, 150, "img/amogus.xbm")
+graphics.draw_character8x8(83, 40, "A")
+graphics.draw_string8x8(0, 0, "amogus")
 
 graphics.refresh()
 
