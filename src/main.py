61daf41827423a18031f93b34c0bbdde36fa7e70
Johnny Stene <johnny@stene.xyz>
2025-01-20 19:41:46 -0700

    homescreen cleanup

--- src/main.py
+++ src/main.py
@@ -29,30 +29,10 @@ while True:
     
     cell_status = cell.phone_status()
     print(cell_status)
-    if("4" in cell_status):
-        # in call
-        pass
-    elif("3" in cell_status):
+    if("3" in cell_status):
         graphics.draw_image(0, 0, "img/ring.xbm")
         graphics.refresh()
         cell.answer_call()
         time.sleep(10)
         cell.end_call()
-
-    
-
-
-graphics.draw_image(0, 0, "img/homescreen.xbm")
-graphics.draw_image(0, 0, "img/amogus.xbm")
-graphics.draw_image(32, 0, "img/bat_empty.xbm")
-
-graphics.draw_image(0, 216, "img/app_empty.xbm")
-
-graphics.draw_string8x8(0, 38, cell.manufacturer)
-graphics.draw_string8x8(0, 46, cell.model)
-graphics.draw_string8x8(0, 54, cell.imei)
-graphics.draw_string8x8(0, 62, cell.phone_number())
-
-graphics.refresh()
-
-print("done")
+        home_redraw = True
