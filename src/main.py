d20cb8d488d42df3702d1dc8615567b4ac59410d
Johnny Stene <johnny@stene.xyz>
2025-01-31 22:46:03 -0700

    busy wait event and main.py docs

--- src/main.py
+++ src/main.py
@@ -35,21 +35,29 @@ def home_redraw():
     graphics.refresh()
 
 def handle_call():
+    # TODO: the way this function is designed is kinda terrible... it assumes the state will never be anything other than "ringing" or "in call"
+    # ... but even when the caller hangs up before we have the chance to answer it just drops home so maybe fine?
+    # TODO: busy waiting in this function will cause infinite loop with it detecting calls... add cell state machine?
+    # TODO: hey google what is a state machine?
     print("incoming call")
     graphics.clear()
     graphics.draw_image(0, 0, "img/ring.xbm")
     graphics.draw_string8x8(0, 32, "A: Answer")
     graphics.draw_string8x8(0, 40, "B: End")
     graphics.refresh()
+
     while True:
+        event.publish("busy_wait")
         response = keypad.get_key(wait=False)
         if response == "A":
             cell.answer_call()
             break
         elif response == "B":
             cell.end_call()
+            event.publish("home_redraw")
             return
-        if not("3" in cell.phone_status()):
+        if not("3" in cell.phone_status()): # 3 is ringing
+            event.publish("home_redraw")
             return
         time.sleep(0.05)
 
@@ -60,20 +68,24 @@ def handle_call():
     cell.answer_call()
 
     while True:
+        event.publish("busy_wait")
         response = keypad.get_key(wait=False)
         if response == "B":
             cell.end_call()
+            event.publish("home_redraw")
             return
-        if not("4" in cell.phone_status()):
+        if not("4" in cell.phone_status()): # 4 is in call
+            event.publish("home_redraw")
             return
         time.sleep(0.05)
 
-def handle_sms(**kwargs):
+def handle_sms(**kwargs): # has to be called with data=<msgdata>
     print("incoming sms")
-    if not "data" in kwargs:
+    if not "data" in kwargs: # told you
         print("sms handler error: called without data karg!")
         return
 
+    # TODO: when msg received flag added just trash the whole function and add an unread count
     message = kwargs["data"]
     graphics.clear()
     graphics.draw_image(0, 0, "img/sms.xbm")
@@ -84,6 +96,27 @@ def handle_sms(**kwargs):
     graphics.refresh()
     time.sleep(10)
 
+    event.publish("home_redraw")
+
+def busy_wait():
+    # TODO: we really need to check for URC from the modem here... phone ringing can screw everything up
+    # TODO: state machine for menus/button input? or maybe move housekeeping to an event to be called on loop?
+
+    # check modem status
+    cell_status = cell.phone_status()
+    if "3" in cell_status: # 3 is ringing
+        event.publish("cell_ring")
+
+    # check sms status (probably a better way to do this? can we ask modem for count of REC UNREAD first?)
+    messages = cell.read_all_sms()
+    for message in messages:
+        if message.status == "REC UNREAD":
+            db.add_message_entry(message) 
+            db.save("db.json") # this will probably never be called more than once per loop so its fine here
+            event.publish("cell_sms", data=message) # will probably have to add read flag to messages in the db so we can just have an unread msg count
+
+# subscribe all handlers to their respective events (TODO: rename functions?)
+event.subscribe("busy_wait", busy_wait)
 event.subscribe("home_redraw", home_redraw)
 event.subscribe("cell_ring", handle_call)
 event.subscribe("cell_sms", handle_sms)
@@ -91,17 +124,7 @@ event.subscribe("cell_sms", handle_sms)
 # Main loop
 home_redraw()
 while True:
-    cell_status = cell.phone_status()
-    if "3" in cell_status:
-        event.publish("cell_ring")
-        event.publish("home_redraw")
-
-    messages = cell.read_all_sms()
-    for message in messages:
-        if message.status == "REC UNREAD":
-            db.add_message_entry(message)
-            db.save("db.json") # this will probably never be called more than once per loop so its fine here
-            event.publish("cell_sms", data=message)
-            event.publish("home_redraw")
+    event.publish("busy_wait")
     
+    # app selection will go here eventually
     key_pressed = keypad.get_key(wait=False)
